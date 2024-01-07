59784d6274c552d1e8044aec3710e9eab9fdabe9
yanglsh <yanglsh@shanghaitech.edu.cn>
2024-01-07 09:22:21 -0700

    change reward

--- hsup/hsup/envs/__init__.py
+++ hsup/hsup/envs/__init__.py
@@ -31,19 +31,23 @@ class HeatSupplyEnv(Env):
             else:
                 self.start_time = pd.Timestamp(start_time).ceil('H')  # Round start_time to the nearest hour
             self.end_time = self.data['indoor'].dropna().index[-1].floor('H')
+
             self.model_indoor = model_indoor
             self.model_sec_back_t = model_sec_back_t
 
-            from dataset.preprocess import X_indoor_columns, X_sec_back_t_columns
             self.X_indoor_cols: list[str] = X_indoor_columns
             self.X_sec_back_t_cols: list[str] = X_sec_back_t_columns
             self.X_cols = list(set(self.X_indoor_cols) | set(self.X_sec_back_t_cols) | {'indoor'})
+
             self.observation_cols = list((set(self.X_indoor_cols) | set(self.X_sec_back_t_cols)) - {'sec_back_t', 'sec_supp_t'})
             self.observation_space = Box(low=-np.inf, high=np.inf, shape=(len(self.observation_cols),))
+            
+            self.discretize = discretize
             if discretize:
-                self.action_space = Discrete(5, start=-2)
+                self.action_space = Discrete(21, start=-10)
             else:
                 self.action_space = Box(low=-2, high=2, shape=(1,))
+
             self.reset()
 
     def reset(self, seed=None, *args, **kwargs) -> tuple[np.ndarray, dict]:
@@ -63,11 +67,15 @@ class HeatSupplyEnv(Env):
     def step(self, A: Action) -> tuple[np.ndarray, Reward, bool, bool, dict]:
         # Apply the action to the environment
         # Return the next state, reward, done (whether the episode is finished), and additional info
-        self.S = next_state = self._get_next_state(A)
-        reward = self._get_reward(next_state)
-        done = not max(0, next_state['outdoor_10min']) < next_state['sec_supp_t_10min'] < 100
+        next_state, indoors = self._get_next_state(A)
+
+        reward = self._get_reward(indoors)
+        done = next_state['sec_supp_t_10min'] > 50 or next_state['sec_supp_t_10min'] < max(0, next_state['outdoor'])
         truncated = self.T == self.end_time
+        
+        self.S = next_state
         assert self.S.name == self.T
+
         return next_state.values.astype(np.float32), reward, done, truncated, {}
 
     def render(self) -> None:
@@ -83,11 +91,17 @@ class HeatSupplyEnv(Env):
 
     def _get_next_state(self, A: Action) -> State:
         
-        assert abs(A) <= 2, f'Action must be between -2 and 2, but got {A}'
-
+        if not self.discretize:
+            assert abs(A) <= 2, f'Action must be between -2 and 2, but got {A}'
+        else:
+            low, n = self.action_space.start, self.action_space.n
+            assert abs(low) == low + n - 1, f'Action space must be symmetric, but got {low} and {low + n - 1}'
+            A = A / ((low + n - 1) / 2)
+            
         next_supp = self.X.loc[self.T, 'sec_supp_t_10min'] + A
+        indoors = []
 
-        for i in range(6):
+        for _ in range(6):
             
             T = self.T
             T_ = T + H
@@ -114,11 +128,14 @@ class HeatSupplyEnv(Env):
             self.X.loc[T_, 'indoor_10min'] = \
             self.X.loc[T__, 'indoor_20min'] = \
             self.X.loc[T___, 'indoor_30min'] = y
-
+            
             self.T += H
+            indoors.append(y.squeeze())
 
             assert not np.isnan(self.X.loc[self.T, self.observation_cols]).any()
-        return self.X.loc[self.T, self.observation_cols]
 
-    def _get_reward(self, S: State) -> Reward:
-        return 4 - (S['indoor_10min'] - 22) ** 2 - max((S['sec_supp_t_10min'] - 40) ** 2 / 100 - 1, 0)
+        return self.X.loc[self.T, self.observation_cols], indoors
+
+    def _get_reward(self, indoors: list[float]) -> Reward:
+        criteria = lambda x: 4 - (x - 22) ** 2 - max((x - 40) ** 2 / 100 - 1, 0)
+        return sum(criteria(x) for x in indoors)
--- train.py
+++ train.py
@@ -7,23 +7,53 @@ np.random.seed(0)
 
 import hsup as _
 import gymnasium as gym
-from stable_baselines3 import PPO
 
-from dataset.preprocess import train_data
-from dataset.simulation import MLPModel
+import dataset.preprocess as preprocess
+import dataset.simulation as simulation
+import dataset.data as data
 
-model_sec_back_t = MLPModel.load_from_checkpoint('lightning_logs/split.sec_back_t/checkpoints/epoch=816-step=90687.ckpt').eval()
-model_indoor = MLPModel.load_from_checkpoint('lightning_logs/split.indoor/checkpoints/epoch=814-step=90465.ckpt').eval()
+max_eposide_steps = 24
+discretize = False
 
-model_sec_back_t.requires_grad_(False)
-model_indoor.requires_grad_(False)
+
+model_sec_back_t = simulation.MLPModel.load_from_checkpoint("lightning_logs/split.sec_back_t/checkpoints/epoch=816-step=90687.ckpt")
+model_indoor = simulation.MLPModel.load_from_checkpoint("lightning_logs/split.indoor/checkpoints/epoch=814-step=90465.ckpt")
+
+model_sec_back_t.eval()
+model_indoor.eval()
 
 env = gym.make(
     "hsup/HeatSupply",
-    data=train_data,
+    max_episode_steps=max_eposide_steps,
+    data=data.train_data,
+    model_sec_back_t=model_sec_back_t,    
     model_indoor=model_indoor,
-    model_sec_back_t=model_sec_back_t,
+    discretize=discretize,
 )
 
-model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=10, progress_bar=True)
+import stable_baselines3
+import torch
+from stable_baselines3 import DQN, PPO, SAC
+
+# model = DQN("MlpPolicy", env, 
+#             verbose=1, 
+#             tensorboard_log="./runs", 
+#             seed=0,
+#             policy_kwargs=dict(
+#                 activation_fn=torch.nn.ReLU,
+#                 net_arch=[64, 64, 64, 64],
+#             ))
+
+model = SAC("MlpPolicy", env, 
+            verbose=1, 
+            tensorboard_log="./runs", 
+            seed=0)
+
+total_timesteps = int(1e4)
+log_interval = 1
+
+model_class = model.__class__.__name__
+
+
+model.learn(total_timesteps=total_timesteps, progress_bar=True, log_interval=log_interval)
+model.save(f"checkpoints/{model_class}-{total_timesteps}")
